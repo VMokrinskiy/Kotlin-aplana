1317d1990acebc05b049baddb4f8ebc2aa7854fc
VMokrinskiy <31728543+VMokrinskiy@users.noreply.github.com>
2020-02-15 16:18:55 +0300

    Task 21

--- src/Module4/FlightCompany.kt
+++ src/Module4/FlightCompany.kt
@@ -1,7 +1,6 @@
 package Module4
 
 fun main(Args: Array<String>){
-    var flight: AirCraft = AirCraft(2, 132.3, 10.2)
     var boeing747: Boeing747 = Boeing747(1,2,12.0,23.2)
     println(boeing747.numOfPassengers)
 }
@@ -10,7 +9,7 @@ open interface Passenger{
     val numOfPassengers:Int
 }
 
-open class AirCraft(numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) {
+abstract class AirCraft(numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) {
     var numberOfPlane: Int = 1
     var maxFlightDistance: Double = 1000.0
     var maxCapacityOfFuel: Double = 15.9
