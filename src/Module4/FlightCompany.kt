80552b7a3f5502a9ae8787c29024469a061f58f4
VMokrinskiy <31728543+VMokrinskiy@users.noreply.github.com>
2020-02-15 15:39:44 +0300

    Task 18

--- src/Module4/FlightCompany.kt
+++ src/Module4/FlightCompany.kt
@@ -1,14 +1,19 @@
 package Module4
 
 fun main(Args: Array<String>){
-    var flight: AirCraft = AirCraft()
+    var flight: AirCraft = AirCraft(2, 132.3, 10.2)
     println(flight.fuelConsamption)
 }
 
-class AirCraft() {
+class AirCraft(numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) {
     var numberOfPlane: Int = 1
     var maxFlightDistance: Double = 1000.0
     var maxCapacityOfFuel: Double = 15.9
+    init{
+        this.numberOfPlane = numberOfPlane
+        this.maxFlightDistance = maxFlightDistance
+        this.maxCapacityOfFuel = maxCapacityOfFuel
+    }
     var fuelConsamption: Double = 2.3
         get() = (maxCapacityOfFuel * 100 / maxFlightDistance)
 }
