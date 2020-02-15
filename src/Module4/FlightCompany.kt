a97c8de676d6458bd3c452fce67a534a8e23fc4b
VMokrinskiy <31728543+VMokrinskiy@users.noreply.github.com>
2020-02-15 16:33:19 +0300

    Task 22

--- src/Module4/FlightCompany.kt
+++ src/Module4/FlightCompany.kt
@@ -3,6 +3,7 @@ package Module4
 fun main(Args: Array<String>){
     var boeing747: Boeing747 = Boeing747(1,2,12.0,23.2)
     println(boeing747.numOfPassengers)
+    boeing747.printInfo()
 }
 
 open interface Passenger{
@@ -10,21 +11,35 @@ open interface Passenger{
 }
 
 abstract class AirCraft(numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) {
-    var numberOfPlane: Int = 1
-    var maxFlightDistance: Double = 1000.0
-    var maxCapacityOfFuel: Double = 15.9
+    protected var numberOfPlane: Int = 1
+    protected var maxFlightDistance: Double = 1000.0
+    protected var maxCapacityOfFuel: Double = 15.9
     init{
         this.numberOfPlane = numberOfPlane
         this.maxFlightDistance = maxFlightDistance
         this.maxCapacityOfFuel = maxCapacityOfFuel
     }
-    var fuelConsamption: Double = 2.3
+    protected var fuelConsamption: Double = 2.3
         get() = (maxCapacityOfFuel * 100 / maxFlightDistance)
+    open fun printInfo(){
+        println("Номер самолета - " + numberOfPlane)
+        println("Максимальная дистанция полета - " + maxFlightDistance)
+        println("Вместимость топлива - "+ maxCapacityOfFuel)
+        println("Расход топлива на 100км - "+fuelConsamption)
+    }
+
 }
 
-class Boeing747(numOfPassengers:Int, numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) : Passenger{
-    override val numOfPassengers:Int
+class Boeing747(numOfPassengers:Int, numberOfPlane:Int, maxFlightDistance:Double, maxCapacityOfFuel:Double) : AirCraft(numberOfPlane, maxFlightDistance, maxCapacityOfFuel), Passenger{
+    override var numOfPassengers: Int = 0
     init{
         this.numOfPassengers = numOfPassengers
+    }
+    override fun printInfo() {
+        println("Количество пассажиров - "+numOfPassengers)
+        println("Номер самолета - " + numberOfPlane)
+        println("Максимальная дистанция полета" + maxFlightDistance)
+        println("Вместимость топлива - "+ maxCapacityOfFuel)
+        println("Расход топлива на 100км"+fuelConsamption)
     }
 }
